cdd70c5a640200f0fe0da82e9093a96c44464459
subhashsathupalli <32120320+subhashsathupalli@users.noreply.github.com>
2024-04-08 10:11:27 +0530

    Add files via upload

---
+++ array_methods.js
@@ -0,0 +1,21 @@
+const arr = [10,20,'i am learning js'];
+arr.push(30,40,30,50);
+console.log(arr);
+
+arr.pop();
+console.log(arr);
+
+console.log(arr[2].split(" ",3));
+console.log(arr.join("_"));
+
+arr.forEach((a)=>{console.log(a*2)});
+arr.forEach((n,i,arr)=>{console.log(n*2, i, arr)});
+
+arr.filter((n)=>{console.log(n+2)});
+
+arr.filter((n)=>(n%2==0)).map(n=>{console.log(n*2)});
+
+arr.some((n)=>{console.log(n+2)});
+
+console.log(arr.indexOf(30));
+console.log(arr.lastIndexOf(30));
